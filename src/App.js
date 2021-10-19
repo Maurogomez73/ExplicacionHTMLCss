6947fa2696661611469034d63fdf6b8ef86dce63
Mauricio Gómez <maox11@gmail.com>
2021-10-18 19:24:58 -0500

    agregar instalción de React (fusionando html y css)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './media/Logo_Soft.png';
+import './styles/styles.css';
+import borderCollie from './media/BorderCollie.jpg';
+import sammy from './media/Sammy.jpeg';
+import rhodesian from './media/Rhodesian.jpg';
+import sanBernardo from './media/San bernardo.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <ul className="navbar">
+            <li>
+                <img src={logo} alt="imagen logo empresa" className="logo"/>
+            </li>
+            <li>
+                <button className="button mainButton">Nuevo post</button>
+            </li>
+            <li>
+                <div className="buscar">
+                    <input placeholder="Buscar una raza"/>
+                    <i className="fas fa-search button iconoBusqueda"></i>
+                </div>
+            </li>
+            <li><button className="button secondaryButton">Login</button></li>
+            <li><button className="button mainButton">Registro</button></li>
+        </ul>
+        
+    </header>
+  
+    <main>
+        <section>
+            <h1>Razas de Perros</h1>
+            <ul className="breedCardContainer">
+                <li className="breedCard">
+                    <div className="contenedorImagen">
+                        <img src={borderCollie} alt="Border Collie" />
+                    </div>
+                    <span className="breedTitle">Border Collie</span>
+                </li>
+                <li className="breedCard">
+                    <div className="contenedorImagen">
+                        <img src={sammy} alt="Schnauzer" />
+                    </div>
+                    <span className="breedTitle">Schnauzer</span>
+                </li>
+                <li className="breedCard">
+                    <div className="contenedorImagen">
+                        <img src={rhodesian} alt="Rhodesian" />
+                    </div>
+                    <span className="breedTitle">Rhodesian</span>
+                </li>
+                <li className="breedCard">
+                    <div className="contenedorImagen">
+                        <img src={sanBernardo} alt="San Bernardo" />
+                    </div>
+                    <span className="breedTitle">San Bernardo</span>
+                </li>
+                
+            </ul>
+        </section>
+        <section></section>
+    </main>
+
+    <footer></footer>
+
     </div>
   );
 }
